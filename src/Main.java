a3798c2a51113426abca6fcdf99458f35a33db5f
SebastienGllmt <SebastienGllmt@gmail.com>
2014-07-12 18:15:55 -0700

    IntSupplier magic

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.util.function.Supplier;
+import java.util.function.IntSupplier;
 
 import javax.imageio.ImageIO;
 
@@ -13,9 +13,9 @@ public class Main {
 	private static final String PREFIX = "<b><font size=\"4\">";
 	private static final String SUFFIX = "</font></b>";
 	
-	private static final Supplier<BlackPixel> BLACK_PIXEL_SUPPLIER = BlackPixel::new;
-	private static final Supplier<WhitePixel> WHITE_PIXEL_SUPPLIER = WhitePixel::new;
-	private static final Supplier<UnknownPixel> UNKNOWN_PIXEL_SUPPLIER = UnknownPixel::new;
+	private static final IntSupplier BLACK_PIXEL_SUPPLIER = () -> '麤';		// the char that looked the most like a black box while being monosized w/ other Chinese/Japanese symbols
+	private static final IntSupplier WHITE_PIXEL_SUPPLIER = () -> '一';		// the char that looked the most empty while being monosized w/ other Chinese/Japanese symbols
+	private static final IntSupplier UNKNOWN_PIXEL_SUPPLIER = () -> '何';	// the char representing a non-black/white pixel
 	
 	public static void main(String[] args) throws IOException {
 		File f = new File(PATH);
@@ -25,7 +25,7 @@ public class Main {
 		
 		BufferedImage img = ImageIO.read(f);
 		
-		Supplier<? extends Pixel> pixelCreator = null;
+		IntSupplier pixelCreator = null;
 		
 		System.out.println(PREFIX);
 		for (int y = 0; y < img.getHeight(); y++) {
@@ -40,35 +40,10 @@ public class Main {
 					pixelCreator = UNKNOWN_PIXEL_SUPPLIER;
 				}
 				
-				System.out.print(pixelCreator.get().getPixel());
+				System.out.print((char) pixelCreator.getAsInt());
 			}
 			System.out.println();
 		}
 		System.out.print(SUFFIX);
-	}
-}
-
-abstract class Pixel {
-	public abstract char getPixel();
-}
-
-class BlackPixel extends Pixel {
-	public char getPixel() {
-		// the char that looked the most like a black box while being monosized w/ other Chinese/Japanese symbols
-		return '麤';
-	}
-}
-
-class WhitePixel extends Pixel {
-	public char getPixel() {
-		// the char that looked the most empty while being monosized w/ other Chinese/Japanese symbols
-		return '一';
-	}
-}
-
-class UnknownPixel extends Pixel {
-	public char getPixel() {
-		// the char representing a non-black/white pixel
-		return '何';
 	}
 }
