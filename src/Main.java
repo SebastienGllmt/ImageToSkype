50fe75a61f22044c5ddb83cca0148b8a043de7ee
SebastienGllmt <SebastienGllmt@gmail.com>
2014-07-12 22:37:31 -0700

    Now gray-scales images Also stops writing if message is too long

--- src/Main.java
+++ src/Main.java
@@ -1,30 +1,35 @@
-import java.awt.Color;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.util.function.IntSupplier;
 
 import javax.imageio.ImageIO;
 
 public class Main {
 	
 	private static final String PATH = "res/img.bmp";
-	private static final String PREFIX = "<b><font size=\"4\">";
+	private static final String PREFIX = "<b><font color=\"#AAAAAA\" size=\"4\">";
 	private static final String SUFFIX = "</font></b>";
 	
+	// These are weights on the rgb scale to turn them into luminance. These seemingly magic numbers apparently make the result "account for human perception" based off how our eyes perceive colors differntly.
+	private static final double WEIGHT_RED = 0.299;
+	private static final double WEIGHT_GREEN = 0.587;
+	private static final double WEIGHT_BLUE = 0.114;
+	
 	/**
-	 * Char that looked the most like a black box while being monosized w/ other Chinese/Japanese symbols
+	 * From trials, this seems to be around the max message length
 	 */
-	private static final IntSupplier BLACK_PIXEL_SUPPLIER = () -> '麤';
+	private static final int MAX_MSG_LENGTH = 2650;
+	
+	private final static int NUM_SHADES = 8;
 	/**
-	 * Char that looked the most empty while being monosized w/ other Chinese/Japanese symbols
+	 * The number we have to divide luminance by to get a number between 0-NUM_SHADES
+	 * For example, if we pick <code>NUM_SHADES</code> as 1, we get
+	 * LUMINANCE_FLATTED = 256;
+	 * 
+	 * With this, any luminance / 256 we get will return 0, which is our single possible number.
 	 */
-	private static final IntSupplier WHITE_PIXEL_SUPPLIER = () -> '一';
-	/**
-	 * Char representing a non-black/white pixel
-	 */
-	private static final IntSupplier UNKNOWN_PIXEL_SUPPLIER = () -> '何';
+	private final static int LUMINANCE_FLATTEN = (256 / NUM_SHADES);
 	
 	public static void main(String[] args) throws IOException {
 		File f = new File(PATH);
@@ -34,25 +39,60 @@ public class Main {
 		
 		BufferedImage img = ImageIO.read(f);
 		
-		IntSupplier pixelCreator = null;
+		int count = 0;
 		
 		System.out.println(PREFIX);
 		for (int y = 0; y < img.getHeight(); y++) {
 			for (int x = 0; x < img.getWidth(); x++) {
-				int color = img.getRGB(x, y);
+				int rgb = img.getRGB(x, y);
+				int luminance = getLuminanceFromRGB(rgb);
+				char pixel = getCharFromLuminance(luminance);
 				
-				if (color == Color.BLACK.getRGB()) {
-					pixelCreator = BLACK_PIXEL_SUPPLIER;
-				} else if (color == Color.WHITE.getRGB()) {
-					pixelCreator = WHITE_PIXEL_SUPPLIER;
-				} else {
-					pixelCreator = UNKNOWN_PIXEL_SUPPLIER;
+				System.out.print(pixel);
+				count++;
+				if (count >= MAX_MSG_LENGTH) {
+					return;
 				}
-				
-				System.out.print((char) pixelCreator.getAsInt());
 			}
 			System.out.println();
 		}
 		System.out.print(SUFFIX);
+	}
+	
+	private static int getLuminanceFromRGB(int rgb) {
+		// First extract colors
+		int red = (rgb >> 16) & 0xFF;
+		int green = (rgb >> 8) & 0xFF;
+		int blue = rgb & 0xFF;
+		
+		// Now multiply by weights
+		red *= WEIGHT_RED;
+		green *= WEIGHT_GREEN;
+		blue *= WEIGHT_BLUE;
+		
+		return (red + green + blue) / LUMINANCE_FLATTEN;
+	}
+	
+	private static char getCharFromLuminance(int luminance) {
+		switch (luminance) {
+			case 0:
+				return '麤';
+			case 1:
+				return '璽';
+			case 2:
+				return '目';
+			case 3:
+				return '王';
+			case 4:
+				return '干';
+			case 5:
+				return '三';
+			case 6:
+				return '二';
+			case 7:
+				return '一';
+			default:
+				return '何';
+		}
 	}
 }
