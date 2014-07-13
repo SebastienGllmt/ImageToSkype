adf5ae31fce25e8e6b0f1a93b6c5cba30099f652
SebastienGllmt <SebastienGllmt@gmail.com>
2014-07-13 00:18:59 -0700

    Added dithering

--- src/Main.java
+++ src/Main.java
@@ -16,6 +16,12 @@ public class Main {
 	private static final double WEIGHT_GREEN = 0.587;
 	private static final double WEIGHT_BLUE = 0.114;
 	
+	// Quant errors based on Floyd-Steinberg dithering ( http://en.wikipedia.org/wiki/Floyd–Steinberg_dithering ) 
+	private static final double QUANT_ERROR_RIGHT = 7.0 / 16;
+	private static final double QUANT_ERROR_DOWN = 5.0 / 16;
+	private static final double QUANT_ERROR_DOWN_LEFT = 3.0 / 16;
+	private static final double QUANT_ERROR_DOWN_RIGHT = 1.0 / 16;
+	
 	/**
 	 * From trials, this seems to be around the max message length
 	 */
@@ -37,17 +43,35 @@ public class Main {
 			throw new FileNotFoundException(f.getAbsolutePath() + " could not be found");
 		}
 		
-		BufferedImage img = ImageIO.read(f);
+		BufferedImage bufferedImage = ImageIO.read(f);
 		
 		int count = 0;
+		int[][] pixels = getPixelArray(bufferedImage);
 		
 		System.out.println(PREFIX);
-		for (int y = 0; y < img.getHeight(); y++) {
-			for (int x = 0; x < img.getWidth(); x++) {
-				int rgb = img.getRGB(x, y);
-				int luminance = getLuminanceFromRGB(rgb);
-				char pixel = getCharFromLuminance(luminance);
+		for (int y = 0; y < bufferedImage.getHeight(); y++) {
+			for (int x = 0; x < bufferedImage.getWidth(); x++) {
 				
+				// Get pixel and luminance
+				int originalPixel = pixels[y][x];
+				int luminance = getLuminanceFromRGB(originalPixel);
+				
+				// Calculate error caused from palette reduction
+				int colorReducedPixel = colorFromRGB(luminance, luminance, luminance);
+				int[] colorError = getColorDifference(originalPixel, colorReducedPixel);
+				
+				// Adjust next pixels based on Floyd-Steinberg dithering ( http://en.wikipedia.org/wiki/Floyd–Steinberg_dithering )
+				if (x < bufferedImage.getWidth() - 1)
+					FixQuantError(pixels, x + 1, y, colorError, QUANT_ERROR_RIGHT);
+				if (y < bufferedImage.getHeight() - 1)
+					FixQuantError(pixels, x, y + 1, colorError, QUANT_ERROR_DOWN);
+				if (x > 0 && y < bufferedImage.getHeight() - 1)
+					FixQuantError(pixels, x - 1, y + 1, colorError, QUANT_ERROR_DOWN_LEFT);
+				if (x < bufferedImage.getWidth() - 1 && y < bufferedImage.getHeight() - 1)
+					FixQuantError(pixels, x + 1, y + 1, colorError, QUANT_ERROR_DOWN_RIGHT);
+				
+				// Print char
+				char pixel = getCharFromLuminance(luminance / LUMINANCE_FLATTEN);
 				System.out.print(pixel);
 				count++;
 				if (count >= MAX_MSG_LENGTH) {
@@ -59,18 +83,74 @@ public class Main {
 		System.out.print(SUFFIX);
 	}
 	
+	private static int[][] getPixelArray(BufferedImage bufferedImage) {
+		int[][] pixels = new int[bufferedImage.getHeight()][bufferedImage.getWidth()];
+		
+		for (int y = 0; y < bufferedImage.getHeight(); y++) {
+			for (int x = 0; x < bufferedImage.getWidth(); x++) {
+				pixels[y][x] = bufferedImage.getRGB(x, y);
+			}
+		}
+		
+		return pixels;
+	}
+	
+	private static void FixQuantError(int[][] pixels, int x, int y, int[] colorError, double quantError) {
+		int[] rgb = getRgbArray(pixels[y][x]);
+		
+		rgb[0] += colorError[0] * quantError;
+		rgb[0] = rgb[0] > 255 ? 255 : rgb[0];
+		
+		rgb[1] += colorError[1] * quantError;
+		rgb[1] = rgb[1] > 255 ? 255 : rgb[1];
+		
+		rgb[2] += colorError[2] * quantError;
+		rgb[2] = rgb[2] > 255 ? 255 : rgb[2];
+		
+		pixels[y][x] = colorFromRGB(rgb[0], rgb[1], rgb[2]);
+		
+	}
+	
+	private static int[] getColorDifference(int rgb1, int rgb2) {
+		int[] rgbArray1 = getRgbArray(rgb1);
+		int[] rgbArray2 = getRgbArray(rgb2);
+		
+		int[] rgbResult = new int[3];
+		rgbResult[0] = rgbArray1[0] - rgbArray2[0];
+		rgbResult[0] = rgbResult[0] > 0 ? rgbResult[0] : -rgbResult[0];
+		
+		rgbResult[1] = rgbArray1[1] - rgbArray2[1];
+		rgbResult[1] = rgbResult[1] > 0 ? rgbResult[1] : -rgbResult[1];
+		
+		rgbResult[2] = rgbArray1[2] - rgbArray2[2];
+		rgbResult[1] = rgbResult[1] > 0 ? rgbResult[1] : -rgbResult[1];
+		
+		return rgbResult;
+	}
+	
+	private static int colorFromRGB(int r, int g, int b) {
+		return b + (g << 8) + (r << 16);
+	}
+	
+	private static int[] getRgbArray(int rgb) {
+		int[] rgbArray = new int[3];
+		rgbArray[0] = (rgb >> 16) & 0xFF;
+		rgbArray[1] = (rgb >> 8) & 0xFF;
+		rgbArray[2] = rgb & 0xFF;
+		
+		return rgbArray;
+	}
+	
 	private static int getLuminanceFromRGB(int rgb) {
 		// First extract colors
-		int red = (rgb >> 16) & 0xFF;
-		int green = (rgb >> 8) & 0xFF;
-		int blue = rgb & 0xFF;
+		int[] rgbArray = getRgbArray(rgb);
 		
 		// Now multiply by weights
-		red *= WEIGHT_RED;
-		green *= WEIGHT_GREEN;
-		blue *= WEIGHT_BLUE;
+		rgbArray[0] *= WEIGHT_RED;
+		rgbArray[1] *= WEIGHT_GREEN;
+		rgbArray[2] *= WEIGHT_BLUE;
 		
-		return (red + green + blue) / LUMINANCE_FLATTEN;
+		return (rgbArray[0] + rgbArray[1] + rgbArray[2]);
 	}
 	
 	private static char getCharFromLuminance(int luminance) {
