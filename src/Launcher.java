0025df0d4d864fa050e89e01595fc9466c5db522
SebastienGllmt <SebastienGllmt@gmail.com>
2014-07-19 02:15:36 -0700

    frame resizes based on picture

--- src/Launcher.java
+++ src/Launcher.java
@@ -1,7 +1,8 @@
+import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
 import java.awt.Font;
-import java.awt.GridLayout;
+import java.awt.Graphics;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
 
@@ -9,14 +10,12 @@ import javax.swing.BoxLayout;
 import javax.swing.JEditorPane;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
-import javax.swing.JTextArea;
 import javax.swing.JTextField;
 
 public class Launcher {
 	
 	private static final String DEFAULT_PATH = "res/img.bmp";
 	private static final String NAME = "ImageToSkype";
-	private static final Dimension SIZE = new Dimension(1280, 960);
 	private static final Font SKYPE_FONT = new Font("LucidaSans", Font.BOLD, 6);
 	
 	public static void main(String[] args) throws IOException {
@@ -25,9 +24,7 @@ public class Launcher {
 	
 	public Launcher() throws IOException {
 		JFrame frame = new JFrame(NAME);
-		frame.setPreferredSize(SIZE);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.setResizable(false);
 		
 		JPanel mainGrid = new JPanel();
 		mainGrid.setLayout(new BoxLayout(mainGrid, BoxLayout.Y_AXIS));
@@ -42,21 +39,18 @@ public class Launcher {
 		
 		mainGrid.add(optionsPanel);
 		
-		JPanel imageContentGrid = new JPanel(new GridLayout(1, 2));
-		
 		JEditorPane preview = new JEditorPane();
-		preview.setContentType("text/html");
 		preview.setFont(SKYPE_FONT);
-		
-		JTextArea rawText = new JTextArea();
-		rawText.setFont(SKYPE_FONT);
 		
 		//JEditorPanel 
 		BufferedImage image;
 		try {
 			image = ImageRender.getImage(DEFAULT_PATH);
-			
 			String content = ImageRender.render(image);
+			
+			Dimension stringDimension = getMinimumStringDimension(image.getGraphics(), content, SKYPE_FONT);
+			preview.setMinimumSize(stringDimension);
+			
 			String optionsColorText = optionsColor.getText();
 			
 			String color;
@@ -66,23 +60,41 @@ public class Launcher {
 				color = ImageRender.getBestFitColorCode(image);
 			}
 			
-			String diplay = content.replaceAll("\n", "<br/>");
-			rawText.setText(ImageRender.getFormattedHTML(content, color, optionsSize.getText()));
-			preview.setText(ImageRender.getFormattedHTML(diplay, color, "1"));
+			preview.setText(ImageRender.getFormattedHTML(content, color, optionsSize.getText()));
+			preview.setForeground(Color.decode(color));
 		} catch (IOException e) {
-			rawText.setText("Failed to load image");
 			preview.setText("Failed to load image");
 		}
-		imageContentGrid.add(rawText);
-		imageContentGrid.add(preview);
+		System.out.println(preview.getMinimumSize());
 		
-		mainGrid.add(imageContentGrid);
+		mainGrid.add(preview);
 		
 		frame.getContentPane().add(mainGrid);
+		
+		frame.setMinimumSize(addDimension(optionsPanel.getMinimumSize(), preview.getMinimumSize()));
 		
 		frame.pack();
 		frame.setVisible(true);
 		frame.setLocationRelativeTo(null);
+		frame.setResizable(false);
 		
+	}
+	
+	private Dimension addDimension(Dimension a, Dimension b) {
+		return new Dimension(a.width + b.width, a.height + b.height);
+	}
+	
+	private Dimension getMinimumStringDimension(Graphics g, String content, Font f) {
+		String longestString = "";
+		String[] splitString = content.split("\n");
+		for (String s : splitString) {
+			if (s.length() > longestString.length()) {
+				longestString = s;
+			}
+		}
+		int containerWidth = g.getFontMetrics(SKYPE_FONT).stringWidth(longestString);
+		int containerHeight = g.getFontMetrics(SKYPE_FONT).getHeight() * splitString.length;
+		
+		return new Dimension(containerWidth, containerHeight);
 	}
 }
